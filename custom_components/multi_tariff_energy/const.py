8116c7e4bdfb13c796f7994ee2cc27ea17a99d32
khennessy74-a11y <khennessy74@gmail.com>
2026-09-25 10:51:04 +0100

    Remove obsolete fixed tariff constants

--- custom_components/multi_tariff_energy/const.py
+++ custom_components/multi_tariff_energy/const.py
@@ -10,7 +10,6 @@ CONF_VAT_RATE = "vat_rate"
 CONF_EXPORT_RATE = "export_rate"
 CONF_CURRENCY = "currency"
 CONF_BILLING_DAY = "billing_day"
-CONF_IMPORT_RATES = "import_rates"
 
 DEFAULT_NAME = "Multi Tariff Energy"
 DEFAULT_CURRENCY = "EUR"
@@ -19,15 +18,3 @@ DEFAULT_STANDING_CHARGE = 0.0
 DEFAULT_EXPORT_RATE = 0.0
 DEFAULT_BILLING_DAY = 1
 
-CONF_TARIFF_1_NAME = "tariff_1_name"
-CONF_TARIFF_1_START = "tariff_1_start"
-CONF_TARIFF_1_END = "tariff_1_end"
-CONF_TARIFF_1_RATE = "tariff_1_rate"
-CONF_TARIFF_2_NAME = "tariff_2_name"
-CONF_TARIFF_2_START = "tariff_2_start"
-CONF_TARIFF_2_END = "tariff_2_end"
-CONF_TARIFF_2_RATE = "tariff_2_rate"
-CONF_TARIFF_3_NAME = "tariff_3_name"
-CONF_TARIFF_3_START = "tariff_3_start"
-CONF_TARIFF_3_END = "tariff_3_end"
-CONF_TARIFF_3_RATE = "tariff_3_rate"
