a0d300a1fca12e991b40a986e26f310dcbc4ad7b
khennessy74-a11y <khennessy74@gmail.com>
2026-09-25 09:15:15 +0100

    Add tariff configuration constants

--- custom_components/multi_tariff_energy/const.py
+++ custom_components/multi_tariff_energy/const.py
@@ -18,3 +18,16 @@ DEFAULT_VAT_RATE = 0.0
 DEFAULT_STANDING_CHARGE = 0.0
 DEFAULT_EXPORT_RATE = 0.0
 DEFAULT_BILLING_DAY = 1
+
+CONF_TARIFF_1_NAME = "tariff_1_name"
+CONF_TARIFF_1_START = "tariff_1_start"
+CONF_TARIFF_1_END = "tariff_1_end"
+CONF_TARIFF_1_RATE = "tariff_1_rate"
+CONF_TARIFF_2_NAME = "tariff_2_name"
+CONF_TARIFF_2_START = "tariff_2_start"
+CONF_TARIFF_2_END = "tariff_2_end"
+CONF_TARIFF_2_RATE = "tariff_2_rate"
+CONF_TARIFF_3_NAME = "tariff_3_name"
+CONF_TARIFF_3_START = "tariff_3_start"
+CONF_TARIFF_3_END = "tariff_3_end"
+CONF_TARIFF_3_RATE = "tariff_3_rate"
