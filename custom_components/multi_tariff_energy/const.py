20d38274feef49413d57f8c54ff7e266c4d94144
khennessy74-a11y <khennessy74@gmail.com>
2026-09-25 08:27:37 +0100

    Add custom_components/multi_tariff_energy/const.py

---
+++ custom_components/multi_tariff_energy/const.py
@@ -0,0 +1,20 @@
+"""Constants for the Multi Tariff Energy integration."""
+
+DOMAIN = "multi_tariff_energy"
+
+CONF_IMPORT_ENERGY_ENTITY = "import_energy_entity"
+CONF_EXPORT_ENERGY_ENTITY = "export_energy_entity"
+CONF_SOLAR_ENERGY_ENTITY = "solar_energy_entity"
+CONF_STANDING_CHARGE = "standing_charge"
+CONF_VAT_RATE = "vat_rate"
+CONF_EXPORT_RATE = "export_rate"
+CONF_CURRENCY = "currency"
+CONF_BILLING_DAY = "billing_day"
+CONF_IMPORT_RATES = "import_rates"
+
+DEFAULT_NAME = "Multi Tariff Energy"
+DEFAULT_CURRENCY = "EUR"
+DEFAULT_VAT_RATE = 0.0
+DEFAULT_STANDING_CHARGE = 0.0
+DEFAULT_EXPORT_RATE = 0.0
+DEFAULT_BILLING_DAY = 1
